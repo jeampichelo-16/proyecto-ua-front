41a087e03e62f6876a91490327de6fc7d581a0ee
jeampichelo-16 <alonsotafur60@gmail.com>
2025-05-18 01:55:27 -0500

    fix: adjust comment formatting for consistency in AdminLayout.vue

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-screen">
     <Sidebar :links="adminSidebarLinks" class="bg-[#fff4cc] text-black" />
     
-    <!-- Establece altura y overflow para el contenido principal -->
+    <!--Establece altura y overflow para el contenido principal -->
     <main class="flex-1 bg-[#fafafa] p-6 pt-16 md:pt-0 overflow-y-auto max-h-screen">
       <RouterView />
     </main>
